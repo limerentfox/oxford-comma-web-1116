9b3b543fc9fc335c4f9a600ad1987c88bf0d27c4
limerentfox <christi@limerentfox.com>
2016-10-31 03:16:26 -0400

    oxford-comma lab complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join("")
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_el = "and #{array.pop}"
+    array.push(last_el)
+    array.join(", ")
+  end
 end
